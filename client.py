8f42f0ec3f2432e0a7a4960e54f189a746afa989
pakohappy <pako_happy@msn.com>
2025-01-25 19:53:11 +0100

    client changes

--- client.py
+++ client.py
@@ -31,31 +31,45 @@ def load_config(config_path):
     except KeyError as e:
         logging.error(f"Error: La sección o clave {e} no se encuentra en el archivo config.ini")
         sys.exit(1)
+    except ValueError as e:
+        logging.error(f"Error: Valor inválido en el archivo config.ini: {e}")
+        sys.exit(1)
 
     return server_host, server_port
 
 def run_client(server_host, server_port):
     """
-    Función para ejecutar el cliente.
+    Establece la conexión con el servidor y maneja la comunicación.
     """
-    while True:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_sock:
-            try:
-                client_sock.connect((server_host, server_port))
-                logging.info(f"Conectado al servidor en {server_host}:{server_port}")
+    client_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_sock.settimeout(10)  # Establecer tiempo de espera de 10 segundos
 
-                while True:
-                    message = input("Mensaje a enviar: ")
-                    if message.lower() == 'exit':
-                        logging.info("Cliente desconectado.")
-                        return
-                    client_sock.sendall(message.encode())
-                    data = client_sock.recv(4096)
-                    logging.info(f"Datos recibidos del servidor: {data.decode()}")
-                    print(f"Respuesta del servidor: {data.decode()}")
-            except socket.error as e:
-                logging.error(f"Error de conexión: {e}")
-                time.sleep(1)
+    try:
+        client_sock.connect((server_host, server_port))
+        logging.info(f"Conectado al servidor {server_host}:{server_port}")
+    except socket.timeout:
+        logging.error(f"No se pudo conectar al servidor {server_host}:{server_port} después de 10 segundos.")
+        sys.exit(1)
+    except socket.error as e:
+        logging.error(f"Error al conectar con el servidor: {e}")
+        sys.exit(1)
+
+    try:
+        while True:
+            message = input("Mensaje a enviar: ")
+            if message.lower() == 'exit':
+                logging.info("Cliente desconectado.")
+                client_sock.close()
+                return
+            client_sock.sendall(message.encode())
+            data = client_sock.recv(4096)
+            logging.info(f"Datos recibidos del servidor: {data.decode()}")
+            print(f"Respuesta del servidor: {data.decode()}")
+    except socket.error as e:
+        logging.error(f"Error de conexión: {e}")
+        time.sleep(1)
+    finally:
+        client_sock.close()
 
 def handle_signal(signum, frame):
     logging.info("Cliente detenido.")
